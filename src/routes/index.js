ddfda7bfc64f3c483d086cf684df371a1e350dd2
ASUS_X515\iurif <iurifreire.18@gmail.com>
2023-04-13 22:01:25 -0300

    Construção da estrutura de pastas e dependências

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const router = require('./router');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+
+
+
+app.listen(process.env.PORT || 3000);
