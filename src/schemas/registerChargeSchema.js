494687d64719cb5b82149345ba2cae1fa4eee17d
ronalt augusto <ronaltaugustocs@gmmail.com>
2023-05-03 20:24:35 -0300

    Implementadi funcoes para pagar e editar cobrancas

--- src/schemas/registerChargeSchema.js
+++ src/schemas/registerChargeSchema.js
@@ -8,9 +8,7 @@ const registerChargeSchema = joi.object({
     due_date: joi.string().required().messages({
         'any.required': 'Este campo deve ser preenchido'
     }),
-    value: joi.number().positive().required().messages({
-        'number.base': 'Este campo precisa estar em um formato válido',
-        'number.positive': 'Este campo precisa estar em um formato válido',
+    value: joi.string().required().messages({
         'any.required': 'Este campo deve ser preenchido',
         'string.empty': 'Este campo deve ser preenchido'
     }),
